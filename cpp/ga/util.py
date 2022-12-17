c06f07c0b71fc17ba33b3e216e77cc2a2f6e9ea5
Feargan <38695943+Feargan@users.noreply.github.com>
2022-12-17 16:46:36 +0100

    alternative mutate_by_swap (mutation per gene)

--- cpp/ga/util.py
+++ cpp/ga/util.py
@@ -69,6 +69,19 @@ def mutate_by_swap(offspring, ga_instance: pygad.GA):
 
     return offspring
 
+# def mutate_by_swap(offspring, ga_instance: pygad.GA):
+#     for chromosome_index in range(offspring.shape[0]):
+#         for gene_index_1, gene_1 in enumerate(offspring[chromosome_index]):
+#             if not try_select(threshold=ga_instance.mutation_probability):
+#                 continue
+#             gene_index_2 = numpy.random.choice(range(offspring.shape[1]))
+#             gene_2 = offspring[chromosome_index, gene_index_2]
+#
+#             offspring[chromosome_index, gene_index_1] = gene_2
+#             offspring[chromosome_index, gene_index_2] = gene_1
+#
+#     return offspring
+
 
 def try_select(threshold, low=0.0, high=1.0):
     roulette = numpy.random.uniform(low=low, high=high)
