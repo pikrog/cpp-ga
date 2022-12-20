f7f36d19a0ca0fd7e6b3bcdd5fde1f5ef446200e
Feargan <38695943+Feargan@users.noreply.github.com>
2022-12-20 23:11:58 +0100

    small correction

--- main_tsp.py
+++ main_tsp.py
@@ -9,7 +9,7 @@ def main():
     if len(sys.argv) > 1:
         graph_file = sys.argv[1]
     else:
-        print("usage: python main_euler.py <graph_file>")
+        print("usage: python main_tsp.py <graph_file>")
         exit(-1)
 
     graph = read_graph_from_file(graph_file)
@@ -21,7 +21,6 @@ def main():
     print(f"The best solution path cost is {cost}")
     print(f"The Postman's paths is: {vertex_path}")
     print(f"which contains {len(vertex_path)} vertices")
-    print(edge_path)
 
 
 if __name__ == "__main__":
