a9d76dc8cfebdd696ceb234fa7188d0c0bfe7269
Feargan <38695943+Feargan@users.noreply.github.com>
2022-12-21 17:40:47 +0100

    tests

--- main_tsp.py
+++ main_tsp.py
@@ -1,7 +1,9 @@
 import sys
 
+import numpy
+
 from cpp.ga.tsp import solve, create_matrix, create_template_ga_instance
-from cpp.ga.util import abs_pmx_crossover, abs_pmx_insert
+from cpp.ga.util import abs_pmx_crossover, abs_pmx_insert, mutate_proxy
 from cpp.graphutil import read_graph_from_file
 
 
@@ -21,24 +23,24 @@ def main():
     graph = read_graph_from_file(graph_file)
     matrix = create_matrix(graph)
 
-    while True:
-        ga_instance = create_template_ga_instance(
-            graph, matrix,
-            num_generations=2000, stop_criteria=["saturate_500"],
-            crossover_probability=0.8,
-            mutation_probability=0.025,
-            population_size=100, num_parents_mating=75,
-            keep_elitism=0,
-            keep_parents=2,
-            parent_selection_type="sss",    # sss & tournament - so far the best
-        )
-        vertex_path, cost, edge_path = solve(graph, matrix, ga_instance)
+    # while True:
+    ga_instance = create_template_ga_instance(
+        graph, matrix,
+        num_generations=2000, stop_criteria=["saturate_500"],
+        crossover_probability=0.8,
+        mutation_probability=0.025,
+        population_size=100, num_parents_mating=75,
+        keep_elitism=0,
+        keep_parents=2,
+        parent_selection_type="sss",    # sss & tournament - so far the best
+    )
+    vertex_path, cost, edge_path = solve(graph, matrix, ga_instance)
 
-        print(f"The best solution path cost is {cost}")
-        print(f"The Postman's paths is: {vertex_path}")
-        print(f"which contains {len(vertex_path)} vertices")
+    print(f"The best solution path cost is {cost}")
+    print(f"The Postman's paths is: {vertex_path}")
+    print(f"which contains {len(vertex_path)} vertices")
 
-        ga_instance.plot_fitness()
+    # ga_instance.plot_fitness()
 
 
 if __name__ == "__main__":
